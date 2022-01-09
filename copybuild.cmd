814f6f3a6583a9909c0e171b78228d9005afbd6d
Ryz3D <mirco.heitmann@gmail.com>
2022-01-09 21:05:26 +0100

    fixed copybuild to work with new setup path

--- copybuild.cmd
+++ copybuild.cmd
@@ -2,7 +2,7 @@
 IF "%CLOSED_PATH%"=="" (
     ECHO Set the environment variable CLOSED_PATH
 ) ELSE (
-    RMDIR %CLOSED_PATH%\addons\close\public /s /q
-    md %CLOSED_PATH%\addons\close\public
-    Xcopy /E build %CLOSED_PATH%\addons\close\public
+    RMDIR %CLOSED_PATH%\setup\addons\close\public /s /q
+    md %CLOSED_PATH%\setup\addons\close\public
+    Xcopy /E build %CLOSED_PATH%\setup\addons\close\public
 )
